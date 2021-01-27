8678864fd97e5afe50df03020c68d4331ddcfd0d
WOODGLIM <78022085+WOODGLIM@users.noreply.github.com>
2021-01-27 17:24:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -26,6 +26,16 @@ async def ping(ctx):
     # 接続に必要なオブジェクトを生成
     client = discord.Client()
     
+    @tasks.loop(seconds=60)
+    async def loop():
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '07:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('おはよう')  
+
+    #ループ処理実行
+    loop.start()
     
     await ctx.send('pong')
 
