6c79de7d810d608076d569a7229edb1cff7005f2
WOODGLIM <78022085+WOODGLIM@users.noreply.github.com>
2021-01-27 17:41:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,14 +1,28 @@
 import discord
-import asyncio
+import datetime
 
 client = discord.Client()
+token = os.environ['DISCORD_BOT_TOKEN']
+
 
 @client.event
 async def on_ready():
-    asyncio.ensure_future(greeting_gm())
+    print('時報機が起動しました。')
 
-async def greeting_gm():
-    await client.send_message(channel, 'おはよう')
-    await asyncio.sleep(10)
+@client.event
+async def on_message(message):
+    
+    date = datetime.datetime.now()
+    hour = date.hour
+    min = date.minute
+    
+        if message.author.bot:
+           return
+        if message.content == '何時？':
+            await message.channel.send(str(hour) + '時です。')
+        if message.content == '何分？':
+             await message.channel.send(str(min) + '分です。')
+        if message.content == '何時何分？':
+             await message.channel.send(str(hour) + '時' + str(min) + '分です。')
 
 client.run(token)
