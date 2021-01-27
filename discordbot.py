b7495fd845851f232d57d8dd1b60c8017d014a9b
WOODGLIM <78022085+WOODGLIM@users.noreply.github.com>
2021-01-27 17:18:24 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,24 +1,51 @@
-#ループのおまじない
 import discord
-from datetime import datetime 
+from datetime import datetime
 from discord.ext import tasks
 
-token = os.environ['DISCORD_BOT_TOKEN']
-#チャンネル指定
-CHANNEL_ID = "803619260349677589"
+TOKEN = "**********" #トークン
+CHANNEL_ID = ********** #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
+
+#投稿する日時
+dateTimeList = [
+'2021/01/27 17:20'
+]
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    print('ready')
+
+# 指定時間に走る処理
+async def SendMessage():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('時間だよ')
+
+# 30秒に一回ループ
+@tasks.loop(seconds=30)
+async def time_check():
+    sleepTime = 0
     # 現在の時刻
-    now = datetime.now(JST).strftime('%H:%M')
-    if now == '17:16':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('おはよう')
-#ループ処理実行
-loop.start()
-    
+    now = datetime.now().strftime('%Y/%m/%d %H:%M')
+    if now in dateTimeList :
+        print(now)
+        await SendMessage()
+        #該当時間だった場合は２重に投稿しないよう３０秒余計に待機
+        await asyncio.sleep(30)
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 使用できるコマンド一覧
+    if message.content == '!help':
+        await message.channel.send('現在使用できるコマンドはありません')
+
+#ループ処理
+time_check.start()
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
