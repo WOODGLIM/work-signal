73a4d648bc84ff105350e0f51913523625d0d9f0
WOODGLIM <78022085+WOODGLIM@users.noreply.github.com>
2021-01-27 17:22:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,50 +2,32 @@ import discord
 from datetime import datetime
 from discord.ext import tasks
 
-TOKEN = "DISCORD_BOT_TOKEN" #トークン
-CHANNEL_ID = "803619260349677589" #チャンネルID
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
+
+from discord.ext import commands
+import os
+import traceback
+
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
 
-#投稿する日時
-dateTimeList = [
-'2021/01/27 17:20'
-]
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
 
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    print('ready')
 
-# 指定時間に走る処理
-async def SendMessage():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('時間だよ')
+@bot.command()
+async def ping(ctx):
+    
+    TOKEN = "DISCORD_BOT_TOKEN" #トークン
+    CHANNEL_ID = "803619260349677589" #チャンネルID
+    # 接続に必要なオブジェクトを生成
+    client = discord.Client()
+    
+    
+    await ctx.send('pong')
 
-# 30秒に一回ループ
-@tasks.loop(seconds=30)
-async def time_check():
-    sleepTime = 0
-    # 現在の時刻
-    now = datetime.now().strftime('%Y/%m/%d %H:%M')
-    if now in dateTimeList :
-        print(now)
-        await SendMessage()
-        #該当時間だった場合は２重に投稿しないよう３０秒余計に待機
-        await asyncio.sleep(30)
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 使用できるコマンド一覧
-    if message.content == '!help':
-        await message.channel.send('現在使用できるコマンドはありません')
-
-#ループ処理
-time_check.start()
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+bot.run(token)
