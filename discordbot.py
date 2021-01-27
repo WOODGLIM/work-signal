d06e061e57c9df1459ff09965ed120961b15a46c
WOODGLIM <78022085+WOODGLIM@users.noreply.github.com>
2021-01-27 17:06:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,7 +3,6 @@ import discord
 from discord.ext import tasks
 from datetime import datetime 
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 #チャンネル指定
 CHANNEL_ID = "803619260349677589"
@@ -14,7 +13,7 @@ CHANNEL_ID = "803619260349677589"
 async def loop():
     # 現在の時刻
     now = datetime.now(JST).strftime('%H:%M')
-    if now == '17:05':
+    if now == '17:08':
         channel = client.get_channel(CHANNEL_ID)
         await channel.send('おはよう')
 #ループ処理実行
